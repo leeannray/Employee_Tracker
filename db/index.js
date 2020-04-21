4e0ed74875c4a986c3450e7ba79e56d32a59c194
Lee Ann Norman <leeann.norm@gmail.com>
2020-04-21 14:11:29 -0500

    added comments and fixed SQL syntax

--- db/index.js
+++ db/index.js
@@ -18,33 +18,43 @@ class Database {
   createDepartment(department) {
     return this.connection.query("INSERT INTO department SET ?", department);
   }
-  // Return employee data
+  // Return employee data, keeps only rows from employee/department tables if duplicates in roles table
   findAllEmployees() {
-      return this.connection.query(
-          "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
-      );
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+    );
   }
-    findAllRoles() {
-        return this.connection.query(
-            'SELECT role.id, role.title, department.name, role.salary FROM role LEFT JOIN department on role.department_id = department.id'
-        );
-    }
-    findAllDepartment() {
-		return this.connection.query(
-			' SELECT department.id, department.name, SUM (role.salary) FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name'
-		);
-	}
-	deleteEmployee(employeeId) {
-		return this.connection.query('DELETE FORM  employee WHERE id = ?', employeeId);
-	}
-	deleteRole(roleId) {
-		return this.connection.query('DELETE FORM role WHERE id = ?', roleId);
-		
-	}
-	deleteDepartment(departmentId) {
-		return this.connection.query('DELETE FORM  department WHERE id = ?', departmentId);
-	}
+  // Return all role types
+  findAllRoles() {
+    return this.connection.query(
+      "SELECT role.id, role.title, department.name, role.salary FROM role LEFT JOIN department ON role.department_id = department.id;"
+    );
+  }
+  // Return all department types
+  findAllDepartments() {
+    return this.connection.query(
+      "SELECT department.id, department.name, SUM (role.salary) FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id GROUP BY department.id, department.name;"
+    );
+  }
+  // Delete employee by id (primary key)
+  deleteEmployee(employeeId) {
+    return this.connection.query(
+      "DELETE FORM employee WHERE id = ?",
+      employeeId
+    );
+  }
+  // Delete role by id (primary key)
+  deleteRole(roleId) {
+    return this.connection.query("DELETE FORM role WHERE id = ?", roleId);
+  }
+  // Delete department by id (primary key)
+  deleteDepartment(departmentId) {
+    return this.connection.query(
+      "DELETE FORM department WHERE id = ?",
+      departmentId
+    );
+  }
 }
 
-
+// Export to be used
 module.exports = new Database(connection);
