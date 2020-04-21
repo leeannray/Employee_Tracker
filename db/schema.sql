88d5c0a80557adde454e2d7a77f0764ff3e01d1c
Lee Ann Norman <leeann.norm@gmail.com>
2020-04-21 11:57:08 -0500

    updated schema to index manager and role and delete cascade

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,34 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 -- Creates the "animals_db" database --
 CREATE DATABASE employeeTracker_db;
 
+USE employeeTracker_db;
+
 CREATE TABLE department(
 id INT PRIMARY KEY AUTO_INCREMENT,
-full_name VARCHAR (30) NOT NULL
+full_name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE employee_role(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
     department_id INT(30) NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id)
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees(
+CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES employee_role(id),
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT(30),
-    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id)
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
 
 SELECT * FROM department;
-SELECT * FROM employee_role;
+SELECT * FROM role;
 SELECT * FROM employee;
