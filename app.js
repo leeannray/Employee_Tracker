87bd74cc265c6326596ce6995f212c114c680f2b
Lee Ann Norman <leeann.norm@gmail.com>
2020-04-21 18:04:10 -0500

    fixed connection config

---
+++ app.js
@@ -0,0 +1,6 @@
+const util = require("util");
+const mysql = require("mysql");
+
+var config = require(__dirname + "/db/config.json");
+conssole.log("Database Info", config)
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,58 @@
 -- Drops the employeeTracker_db if it exists currently --
-DROP DATABASE IF EXISTS employeeTracker_db;
+DROP DATABASE IF EXISTS ;
 -- Creates the "employeeTracker_db" database --
 CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
-CREATE TABLE department(
-id INT AUTO_INCREMENT KEY NOT NULL,
-name VARCHAR(30)
+
+
+
+CREATE TABLE department
+(
+    id INT
+    UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR
+    (30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL(10, 4) DEFAULT(0),
-    department_id INT,
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_role FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
+    CREATE TABLE role
+    (
+        id INT
+        UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR
+        (30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind
+        (department_id),
+  CONSTRAINT fk_department FOREIGN KEY
+        (department_id) REFERENCES department
+        (id) ON
+        DELETE CASCADE
 );
 
-CREATE TABLE employee(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT(30),
-    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE,
-    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE CASCADE SET NULL
-);
-
+        CREATE TABLE employee
+        (
+            id INT
+            UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR
+            (30) NOT NULL,
+  last_name VARCHAR
+            (30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind
+            (role_id),
+  CONSTRAINT fk_role FOREIGN KEY
+            (role_id) REFERENCES role
+            (id) ON
+            DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind
+            (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY
+            (manager_id) REFERENCES employee
+            (id) ON
+            DELETE
+            SET NULL
+            );
