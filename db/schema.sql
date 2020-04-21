e79d1bb0690b9a0e0bd98c66bcbbefc3325ad188
Lee Ann Norman <leeann.norm@gmail.com>
2020-04-21 16:25:24 -0500

    added seeds for query SQL

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,31 @@
--- Drops the employeeTracker_DB if it exists currently --
+-- Drops the employeeTracker_db if it exists currently --
 DROP DATABASE IF EXISTS employeeTracker_db;
--- Creates the "animals_db" database --
+-- Creates the "employeeTracker_db" database --
 CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
 CREATE TABLE department(
-id INT PRIMARY KEY AUTO_INCREMENT,
-full_name VARCHAR(30) UNIQUE NOT NULL
+id INT AUTO_INCREMENT KEY NOT NULL,
+name VARCHAR(30)
 );
 
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL(10,4) NOT NULL,
-    department_id INT(30) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10, 4) DEFAULT(0),
+    department_id INT,
     INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
+    CONSTRAINT fk_role FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT NOT NULL,
-    INDEX role_ind (role_id),
     manager_id INT(30),
-    INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE CASCADE SET NULL
+    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE CASCADE SET NULL
 );
 
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+USE employeeTracker_db;
+INSERT INTO department (name)
+VALUES ("Web Development"),
+    ("Full-Stack"),
+    ("Front-End"),
+    ("Back-End"),
+    ("UI/UX");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Senior", 65000, 1),
+("Junior", 55000, 1),
+("Senior", 68000, 2),
+("Team Lead", 100000, 2),
+("Senior", 70000, 3),
+("Intern", 50000, 1),
+("Manager", 125000, 1)
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lee Ann", "Norman", 1, NULL),
+("Jonathan", "Cool", 2, 1),
+("Brian", "Genius", 4, 3),
+("Jorge", "Awesome", 5, NULL),
+("Miguel", "Smart", 6, 6),
+("Robyn", "Nice", 7, 6)
